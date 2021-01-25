4ce6151bde6e2390c84d5b39a6498473a4d5fb8f
Adam Fowler <adamfowler71@gmail.com>
2021-01-25 09:35:02 +0000

    hummingbird-fluent -> HummingbirdFluent

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
         .macOS(.v10_15),
     ],
     products: [
-        .library(name: "hummingbird-fluent", targets: ["hummingbird-fluent"]),
+        .library(name: "HummingbirdFluent", targets: ["HummingbirdFluent"]),
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", .branch("main")),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0")
     ],
     targets: [
-        .target(name: "hummingbird-fluent", dependencies: [
+        .target(name: "HummingbirdFluent", dependencies: [
             .product(name: "Hummingbird", package: "hummingbird"),
             .product(name: "FluentKit", package: "fluent-kit")
         ]),
