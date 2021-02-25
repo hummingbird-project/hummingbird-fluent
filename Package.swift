12b9cc39cea8ffaaea0faaaa70787a040c168b63
Adam Fowler <adamfowler71@gmail.com>
2021-02-25 16:24:57 +0000

    Update to hummingbird v0.5.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "HummingbirdFluent", targets: ["HummingbirdFluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.2.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "0.5.0"),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0"),
     ],
     targets: [
