1e3f978ee96cfcb14d1f77b20b183c7526db38b0
Adam Fowler <adamfowler71@gmail.com>
2021-04-13 16:53:03 +0100

    Comments, renamed persist DB table to "_hb_persist_"

--- Sources/HummingbirdFluent/Persist+fluent.swift
+++ Sources/HummingbirdFluent/Persist+fluent.swift
@@ -16,42 +16,6 @@ import FluentKit
 import Foundation
 import Hummingbird
 
-final class PersistModel: Model {
-    init() {}
-
-    // name of persist table
-    static let schema = "_persist_"
-
-    @ID(custom: "id")
-    var id: String?
-
-    @Field(key: "value")
-    var value: Data
-
-    @Field(key: "expires")
-    var expires: Date?
-
-    init(id: String, value: Data, expires: Date? = nil) {
-        self.id = id
-        self.value = value
-        self.expires = expires
-    }
-}
-
-struct CreatePersistModel: Migration {
-    func prepare(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("_persist_")
-            .field(.id, .string, .identifier(auto: false))
-            .field("value", .data, .required)
-            .field("expires", .datetime)
-            .create()
-    }
-
-    func revert(on database: Database) -> EventLoopFuture<Void> {
-        database.schema("_persist_").delete()
-    }
-}
-
 /// Fluent driver for persist system for storing persistent cross request key/value pairs
 class HBFluentPersistDriver: HBPersistDriver {
     /// Initialize HBFluentPersistDriver
@@ -64,22 +28,25 @@ class HBFluentPersistDriver: HBPersistDriver {
         }
     }
 
+    /// shutdown driver, cancel tidy task
     func shutdown() {
         self.tidyTask?.cancel()
     }
 
+    /// Create new key. This doesn't check for the existence of this key already so may fail if the key already exists
     func create<Object: Codable>(key: String, value: Object, expires: TimeAmount?, request: HBRequest) -> EventLoopFuture<Void> {
         do {
             let db = request.db(self.databaseID)
             let data = try JSONEncoder().encode(value)
             let date = expires.map { Date() + Double($0.nanoseconds) / 1_000_000_000 } ?? Date.distantFuture
-            let model = PersistModel(id: key, value: data, expires: date)
+            let model = PersistModel(id: key, data: data, expires: date)
             return model.save(on: db).map { _ in }
         } catch {
             return request.eventLoop.makeFailedFuture(error)
         }
     }
 
+    /// Set value for key.
     func set<Object: Codable>(key: String, value: Object, expires: TimeAmount?, request: HBRequest) -> EventLoopFuture<Void> {
         do {
             let db = request.db(self.databaseID)
@@ -90,11 +57,11 @@ class HBFluentPersistDriver: HBPersistDriver {
                 .first()
                 .flatMap { model in
                     if let model = model {
-                        model.value = data
+                        model.data = data
                         model.expires = date
                         return model.update(on: db).map { _ in }
                     } else {
-                        let model = PersistModel(id: key, value: data, expires: date)
+                        let model = PersistModel(id: key, data: data, expires: date)
                         return model.save(on: db).map { _ in }
                     }
                 }
@@ -107,13 +74,14 @@ class HBFluentPersistDriver: HBPersistDriver {
         }
     }
 
+    /// Get value for key
     func get<Object: Codable>(key: String, as object: Object.Type, request: HBRequest) -> EventLoopFuture<Object?> {
         return PersistModel.query(on: request.db(self.databaseID))
             .filter(\._$id == key)
             .filter(\.$expires > Date())
             .first()
             .flatMapThrowing {
-                guard let data = $0?.value else { return nil }
+                guard let data = $0?.data else { return nil }
                 return try JSONDecoder().decode(object, from: data)
             }
             .flatMapErrorThrowing { error in
@@ -122,6 +90,7 @@ class HBFluentPersistDriver: HBPersistDriver {
             }
     }
 
+    /// Remove key
     func remove(key: String, request: HBRequest) -> EventLoopFuture<Void> {
         return PersistModel.find(key, on: request.db(self.databaseID))
             .flatMap { model in
@@ -130,6 +99,7 @@ class HBFluentPersistDriver: HBPersistDriver {
             }
     }
 
+    /// tidy up database by cleaning out expired keys
     func tidy() {
         _ = PersistModel.query(on: application.db(self.databaseID))
             .filter(\.$expires < Date())
@@ -143,13 +113,51 @@ class HBFluentPersistDriver: HBPersistDriver {
 
 /// Factory class for persist drivers
 extension HBPersistDriverFactory {
-    /// In memory driver for persist system
+    /// fluent driver for persist system
     public static var fluent: HBPersistDriverFactory {
         .init(create: { app in HBFluentPersistDriver(application: app, databaseID: nil) })
     }
 
-    /// In memory driver for persist system
+    /// fluent driver for persist system using a specific database id
     public static func fluent(_ datebaseID: DatabaseID?) -> HBPersistDriverFactory {
         .init(create: { app in HBFluentPersistDriver(application: app, databaseID: datebaseID) })
+    }
+}
+
+/// Fluent model used to store persist data
+final class PersistModel: Model {
+    init() {}
+
+    // name of persist table
+    static let schema = "_hb_persist_"
+
+    @ID(custom: "id")
+    var id: String?
+
+    @Field(key: "data")
+    var data: Data
+
+    @Field(key: "expires")
+    var expires: Date?
+
+    init(id: String, data: Data, expires: Date? = nil) {
+        self.id = id
+        self.data = data
+        self.expires = expires
+    }
+}
+
+/// Migration for creating persist model
+struct CreatePersistModel: Migration {
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("_hb_persist_")
+            .field(.id, .string, .identifier(auto: false))
+            .field("data", .data, .required)
+            .field("expires", .datetime)
+            .create()
+    }
+
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("_persist_").delete()
     }
 }
