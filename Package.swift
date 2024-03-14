7f3f075b1db45c4b24cce12dc5c92452b98684d9
Adam Fowler <adamfowler71@gmail.com>
2024-03-14 10:45:15 +0000

    Use Hummingbird 2.0.0-beta.1

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", branch: "main"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-beta.1"),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.45.1"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.0.0"),
         // used in tests
